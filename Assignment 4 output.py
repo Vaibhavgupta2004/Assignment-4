8145f75c5ee98aba8b22f2e5d8e2843d01a4d942
Vaibhavgupta2004 <97099676+Vaibhavgupta2004@users.noreply.github.com>
2022-02-27 11:23:49 +0530

    Add files via upload

---
+++ Assignment 4 output.py
@@ -0,0 +1,130 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+====================== RESTART: E:/Python/Assignment 4.py ======================
+
+TOWER OF HANOI
+
+Enter the rod from which to start[A/B/C]:A
+Enter the rod to which the disks need to be moved[A/B/C]:C
+
+Solution of the puzzle:
+Move the 1st disk : from A rod --> C rod.
+Move the 2nd disk : from A rod --> B rod.
+Move the 1st disk : from C rod --> B rod.
+Move the 3rd disk : from A rod --> C rod.
+Move the 1st disk : from B rod --> A rod.
+Move the 2nd disk : from B rod --> C rod.
+Move the 1st disk : from A rod --> C rod.
+--------------------------------------------------------------------------------
+
+PASCAL's TRIANGLE
+
+
+Recursive Procedure
+
+Enter the number of rows you want:8
+                        1                           
+                      1   1                         
+                    1   2   1                       
+                  1   3   3   1                     
+                1   4   6   4   1                   
+             1   5   10   10   5   1                
+          1   6   15   20   15   6   1              
+        1   7   21   35   35   21   7   1           
+
+Iterative Procedure
+
+Enter the number of rows you want:7
+                    1                       
+                  1   1                     
+                1   2   1                   
+              1   3   3   1                 
+            1   4   6   4   1               
+         1   5   10   10   5   1            
+      1   6   15   20   15   6   1          
+--------------------------------------------------------------------------------
+
+QUOTIENT AND REMAINDER
+
+Enter the first integer:14
+Enter the second integer:3
+
+Quotient : 4
+Remainder : 2
+
+Part a
+
+Checking if the function is callable : It's callable
+
+Part b
+
+All the values are non-zero.
+
+Part c
+
+Filtered out values which are greater than 4 : [5, 6]
+
+Part d
+
+Converted Set : {5, 6}
+
+Part e
+
+Immutable Set : frozenset({5, 6})
+
+Part f
+
+Maximum value : 6
+Hash value of max value : 6
+--------------------------------------------------------------------------------
+
+__init__ AND __del__ FUNCTIONS
+
+Name =VAIBHAV GUPTA
+Roll_no = 21104073
+--------------------------------------------------------------------------------
+
+EMPLOYEE DETAILS MANAGER
+
+
+  Details of all the Employees:
+
+Name		Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		40000
+Ashok		5000
+Ashok		60000
+_________________________
+
+Part a : updating records
+
+
+  Details of all the Employees:
+
+Name		Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		70000
+Ashok		5000
+Ashok		60000
+_________________________
+
+Part b : Deleting Records
+
+
+  Details of all the Employees:
+
+Name	Salary
+-*-*-*-*-*-*-*-*-*-*-*-*
+Mehak		70000
+Ashok		5000
+--------------------------------------------------------------------------------
+
+FRIENDSHIP TRUTHFULNESS TESTER
+
+Enter the word entered by George:Teacher
+Enter the word entered by Barbie:Cheater
+
+Result:
+Their Friendship is true.
+--------------------------------------------------------------------------------
